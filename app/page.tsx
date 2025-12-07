5d01c2ae1f4408543f061df130098c6e48323677
unknown <zubaerislam703@gmail.com>
2025-12-08 04:09:36 +0600

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
-import { Link } from "lucide-react";
-import Image from "next/image";
+import Image from "next/image"
+import LoginForm from "./login/LoginForm"
 
-export default function Home() {
+
+export default function LoginPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <Link href="/login" >Login</Link>
+    <div className="flex min-h-screen bg-gradient-to-b bg-white ">
+      
+      {/* Left Side - Login Form */}
+      <div className="w-full md:w-1/2 p-3 xs:p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12 2xl:p-16 flex items-center justify-center bg-white">
+        <div className="w-full max-w-xs xs:max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl">
+          <LoginForm />
+        </div>
+      </div>
+
+      {/* Right Side - Branding */}
+                 <div className="hidden md:block md:w-1/2 relative">
+                      <Image
+                        src="/Group 350.svg"
+                        alt="Side Image"
+                        fill
+                        className="object-cover"
+                        priority
+                        sizes="50vw"
+                      />
+                    </div>
+
     </div>
-  );
+  )
 }
